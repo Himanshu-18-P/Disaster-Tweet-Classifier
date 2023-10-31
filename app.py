440ebdc76aaaf8108de3d1133d3f80b73f2de6da
Himanshu <140253431+Himanshu-18-P@users.noreply.github.com>
2023-10-31 18:09:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import  streamlit as st
 import pickle
 import sklearn
 import  nltk
-from nltk.stem.porter import PorterStemmer
-ps = PorterStemmer()
 import  string
 from nltk.corpus import stopwords
-stopwords.words('english')
+try:
+    stopwords.words('english')
+except LookupError:
+    nltk.download('stopwords')
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+
 cv = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
@@ -15,19 +19,20 @@ st.title('Disaster Tweet Classifier')
 input_sms = st.text_input('Enter the Tweet')
 
 # 1 . preprocess
-if st.button('Predict') :
+if st.button('Predict'):
     def text_trformation(text) :
-           # step 1
-           text = text.lower()
+
+    # step 1
+        text = text.lower()
     # step 2
-           words = nltk.word_tokenize(text)
+        words = nltk.word_tokenize(text)
     # step 3
-           filtered_words = [i for i in words if i.isalnum()]
+        filtered_words = [i for i in words if i.isalnum()]
     # step 4
-           filtered_words = [i for i in filtered_words if i not in stopwords.words('english') and i not in string.punctuation]
+        filtered_words = [i for i in filtered_words if i not in stopwords.words('english') and i not in string.punctuation]
     # step 5
-           stem_words = [ps.stem(i) for i in filtered_words]
-           return ' '.join(stem_words)
+        stem_words = [ps.stem(i) for i in filtered_words]
+        return ' '.join(stem_words)
 
     transformed_sms = text_trformation(input_sms)
 
@@ -43,3 +48,4 @@ if st.button('Predict') :
            st.header('yes it is a disaster tweet')
     else :
             st.header('not a disaster tweet')
+
